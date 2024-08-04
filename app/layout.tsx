66afddcfbd11b76f0c2bd4643a828eee00a04c92
Babina081 <babitamoo333@gmail.com>
2024-08-04 12:47:42 +0545

    update layout css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next Portfolio",
-  description: "practice portfolio using nextjs and tailwindcss",
+  title: "Babina | Personal Portfolio",
+  description: "Babina Gurung is a fullstack developer",
 };
 
 export default function RootLayout({
@@ -15,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}> {children}</body>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem]  -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header></Header>
+        {children}
+      </body>
     </html>
   );
 }
