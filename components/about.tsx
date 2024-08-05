2a77faded2a42450c4253f0a3bfba2c1ff8549b4
Babina081 <babitamoo333@gmail.com>
2024-08-05 11:26:21 +0545

    refactor inview hook in each page on scroll

--- components/about.tsx
+++ components/about.tsx
@@ -2,14 +2,19 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
 const About = () => {
+  const { ref } = useSectionInView("About", 0.75);
+
   return (
     <motion.section
+      ref={ref}
       id="about"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
