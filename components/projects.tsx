a50cccc18b9071205faaf5f4c9d869a5f051c80a
Babina081 <babitamoo333@gmail.com>
2024-08-05 11:45:07 +0545

    add margin bottom

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ import { useSectionInView } from "@/lib/hooks";
 const Projects = () => {
   const { ref } = useSectionInView("Projects", 0.5);
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
