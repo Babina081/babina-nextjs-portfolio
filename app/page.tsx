af2f1db2a846df8ea69ea5f9a68018554e3808e7
Babina081 <babitamoo333@gmail.com>
2024-08-08 11:20:46 +0545

    add components imported

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/about";
+import Academic from "@/components/academic";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
+import GsapAboutMe from "@/components/gsap-about-me";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -11,10 +13,12 @@ export default function Home() {
     <main className="flex flex-col items-center  px-4">
       <Intro></Intro>
       <SectionDivider></SectionDivider>
+      <GsapAboutMe></GsapAboutMe>
       <About></About>
       <Projects></Projects>
       <Skills></Skills>
       <Experience></Experience>
+      <Academic></Academic>
       <Contact></Contact>
     </main>
   );
