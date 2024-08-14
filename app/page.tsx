c09484734ad9cd1e6e6528164fbfa07f1a7ff444
Babina081 <babitamoo333@gmail.com>
2024-08-14 18:45:42 +0545

    remove section divider

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center  px-4">
       <Intro></Intro>
-      <SectionDivider></SectionDivider>
+      {/* <SectionDivider></SectionDivider> */}
       <ScrollText></ScrollText>
       <About></About>
       <Projects></Projects>
