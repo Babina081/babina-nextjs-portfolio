999583c0f489136b00db37a0ec6dc1fd2bc42190
Babina081 <babitamoo333@gmail.com>
2024-08-14 18:44:47 +0545

    update heading color

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -5,7 +5,7 @@ type SectionHeadingProps = {
 
 const SectionHeading = ({ children }: SectionHeadingProps) => {
   return (
-    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+    <h2 className=" capitalize mb-8  text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text dark:from-white dark:to-blue-300">
       {children}
     </h2>
   );
