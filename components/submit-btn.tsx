79fbec85cdb1b9bc2750fa0590f14ea378ac45de
Babina081 <babitamoo333@gmail.com>
2024-08-14 21:10:07 +0545

    update submit button in contact section

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -4,11 +4,10 @@ import { FaPaperPlane } from "react-icons/fa";
 
 const SubmitBtn = () => {
   const { pending } = useFormStatus();
-
   return (
     <button
       type="submit"
-      className="group h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all items-center justify-center gap-2 flex focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 dark:bg-opacity-10 dark:bg-white disabled:bg-opacity-65 disabled:scale-100  "
+      className="group h-[3rem] w-[8rem] transition-all text-white rounded-full outline-none items-center justify-center gap-2 flex focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 dark:bg-opacity-10 disabled:bg-opacity-65 disabled:scale-100 btn-gradient "
     >
       {pending ? (
         <div className="h-5 w-5 rounded-full border-b-2 border-white animate-spin "></div>
