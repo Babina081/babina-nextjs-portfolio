886d5e705553605edb0b45c0d1d5ad2555c64333
Babina081 <babitamoo333@gmail.com>
2024-08-15 21:22:10 +0545

    update text css

--- components/project.tsx
+++ components/project.tsx
@@ -35,8 +35,8 @@ function Project({
     >
       <section className="order-1 sm:group-even:order-2 bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  sm:group-even:pl-8 hover:bg-gray-200 transition rounded-lg dark:bg-white/10 dark:hover:bg-white/20 dark:text-white  ">
         <div className="pt-4 pb-7 px-5  sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed  text-gray-700 dark:text-white/70">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <p className="mt-2 text-sm leading-relaxed  text-gray-700 dark:text-white/70 tracking-tighter">
             {" "}
             {description}
           </p>
