2d1530280ffa6b234714d964ce22f9cf8e4d3d8d
Babina081 <babitamoo333@gmail.com>
2024-08-15 18:08:26 +0545

    add academic datas in academic section

--- components/academic.tsx
+++ components/academic.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef } from "react";
 import SectionHeading from "./section-heading";
 import { motion, useScroll, useTransform } from "framer-motion";
 import capImage from "@/public/cap.png";
+import { academicData } from "@/lib/data";
 
 const Academic = () => {
   // const { ref } = useSectionInView("Academic", 0.75);
@@ -26,40 +27,62 @@ const Academic = () => {
   const translateY = useTransform(scrollYProgress, [0, 1], [200, -200]);
 
   return (
-    <div id="academic" ref={sectionRef} className="scroll-mt-28 mb-28 ">
-      <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
-        Lookover My Educational History
-      </p>
-      <SectionHeading>ACADEMIC CERTIFICATES</SectionHeading>
+    <div id="academic" ref={sectionRef} className="scroll-mt-28 mb-40 ">
+      <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative group">
+        <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
+          Lookover My Educational History
+        </p>
+        <SectionHeading>ACADEMIC CERTIFICATES</SectionHeading>
+        <div className="flex flex-col gap-10">
+          {academicData.map(
+            ({ date, college, degree, location, affiliated }) => (
+              <div className="flex flex-col gap-0 sm:gap-10" key={college}>
+                <div className="paper">
+                  <div className="pin">
+                    <div className="shadow"></div>
+                    <div className="metal"></div>
+                    <div className="bottom-circle"></div>
+                  </div>
+                  <div className="flex  text-gray-700 flex-row-reverse gap-10 items-start p-10 justify-between">
+                    <span className="text-sm bg-gradient-to-b from-black to-blue-700 text-transparent bg-clip-text w-32">
+                      {date}
+                    </span>
+                    <div className="flex flex-col items-start justify-start w-full">
+                      <h1 className="text-sm font-semibold sm:font-bold">
+                        {college}
+                      </h1>
+                      <h3 className="text-sm font-medium sm:font-semibold italic">
+                        {affiliated}
+                      </h3>
+                      <p className="text-sm font-bold underline text-left">
+                        {location}
+                      </p>
+                      <h2 className="font-medium p-1 text-red-700 drop-shadow-md text-sm">
+                        {degree}
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </div>
 
-      <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-        {" "}
-        {/* <motion.img
-            src={capImage.src}
-            alt="cog Image"
-            className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
-            animate={{ translateY: [-30, 30] }}
-            transition={{
-              repeat: Infinity,
-              repeatType: "mirror",
-              duration: 3,
-              ease: "easeInOut",
-            }}
-          ></motion.img> */}
         <motion.img
           src={capImage.src}
           alt="cylinder Image"
           width={220}
           height={220}
-          className="hidden md:block -top-8 -left-32 md:absolute"
+          className="hidden md:block -top-16 -left-32 md:absolute -z-10 drop-shadow-md"
           style={{ translateY: translateY }}
         ></motion.img>
+
         <motion.img
           src={capImage.src}
           alt="noodle Image"
           width={220}
           height={220}
-          className="hidden lg:block top-[524px] left-[448px] md:absolute rotate-[30deg]"
+          className="hidden lg:block top-[600px] left-[448px] md:absolute rotate-[30deg]"
           style={{ rotate: 30, translateY: translateY }}
         ></motion.img>
       </div>
