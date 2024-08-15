3c7f20506da5781d4885114174dfb22bb9c2f711
Babina081 <babitamoo333@gmail.com>
2024-08-15 12:04:35 +0545

    update skill section

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 
 const fadeInAnimationsVariants = {
@@ -21,27 +21,44 @@ const fadeInAnimationsVariants = {
 };
 
 const Skills = () => {
-  const { ref } = useSectionInView("Skills", 0.75);
+  const skillRef = useRef(null);
+  const { ref: sectionInViewRef } = useSectionInView("Home", 0.5);
+
+  // Combine the refs using useEffect
+  useEffect(() => {
+    if (sectionInViewRef) {
+      sectionInViewRef(skillRef.current);
+    }
+  }, [sectionInViewRef]);
+
+  const { scrollYProgress } = useScroll({
+    target: skillRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section
+    <motion.section
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
-      ref={ref}
+      ref={skillRef}
       id="skills"
+      style={{ translateY: translateY }}
     >
       <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
         What Skill I Am Equipped With
       </p>
       <SectionHeading>My Skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+      <ul className="flex flex-wrap justify-center space-x-4 space-y-4 mt-12 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
             variants={fadeInAnimationsVariants}
             initial="initial"
             whileInView="animate"
-            // viewport={{ once: true }}
+            viewport={{ once: true }}
             key={index}
             custom={index}
-            className="bg-white borderBlack rounded-xl px-5 py-3 flex items-center justify-between flex-col gap-2 dark:bg-white/10 dark:text-white/80"
+            className="bg-white borderBlack rounded-xl px-5 py-3 flex items-center justify-between flex-col gap-2 dark:bg-white/10 dark:text-white/80 "
+            style={{ rotate: -20 }}
           >
             <Image
               src={skill.image}
@@ -55,7 +72,7 @@ const Skills = () => {
           </motion.li>
         ))}
       </ul>
-    </section>
+    </motion.section>
   );
 };
 
