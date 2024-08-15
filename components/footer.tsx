cde06168b9822ea973a9757715e0e9d150fe1664
Babina081 <babitamoo333@gmail.com>
2024-08-15 18:13:19 +0545

    update footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,14 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; 2024 Babina. All rights reserved.
+      <small className="mb-2  text-xs flex items-center justify-center">
+        &copy; 2024 Babina Gurung Personal Portfolio 💼. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
     </footer>
   );
 };
