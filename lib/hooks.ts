d1328a9b87c4084cf2959ba5953971d7be7ddeb5
Babina081 <babitamoo333@gmail.com>
2024-08-16 10:37:28 +0545

    update format

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -4,7 +4,9 @@ import { useInView } from "react-intersection-observer";
 import type { SectionName } from "./types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.5) {
-  const { ref, inView } = useInView({ threshold });
+  const { ref, inView } = useInView({
+    threshold,
+  });
 
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
