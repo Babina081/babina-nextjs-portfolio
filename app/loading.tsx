5b8b1ebad2756b4c2d7f769e6f36ff249b464eff
Babina081 <babitamoo333@gmail.com>
2024-08-16 10:39:52 +0545

    add loading component

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="spinner w-screen h-screen items-center justify-center flex"></div>
+  );
+};
+
+export default Loading;
