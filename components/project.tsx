bd0b107a459ec9c417e1547a08cea8c923b925b5
Babina081 <babitamoo333@gmail.com>
2024-08-16 10:39:41 +0545

    fix action section in view of project page

--- components/project.tsx
+++ components/project.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { useScroll, motion, useTransform } from "framer-motion";
 import { FaGithub } from "react-icons/fa";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import Link from "next/link";
+import { useSectionInView } from "@/lib/hooks";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -17,21 +18,17 @@ function Project({
   gitpage,
   demo,
 }: ProjectProps) {
-  const ref = useRef<HTMLDivElement>(null);
+  const projectRef = useRef<HTMLDivElement>(null);
+
   const { scrollYProgress } = useScroll({
-    target: ref,
+    target: projectRef,
     offset: ["0 1", "1.33 1"],
   });
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
-    <motion.div
-      ref={ref}
-      style={{
-        scale: scaleProgress,
-        opacity: opacityProgress,
-      }}
-      className="group mb-3 sm:mb-8 last:mb-0 flex gap-2 sm:flex-row "
+    <
     >
       <section className="order-1 sm:group-even:order-2 bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  sm:group-even:pl-8 hover:bg-gray-200 transition rounded-lg dark:bg-white/10 dark:hover:bg-white/20 dark:text-white  ">
         <div className="pt-4 pb-7 px-5  sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
@@ -63,11 +60,11 @@ function Project({
           {" "}
           <FaGithub className="h-7 w-7 border rounded-full drop-shadow-md  bg-gradient-to-br dark:from-white dark:to-blue-500    cursor-pointer hover:scale-125 transition-all duration-200" />
         </Link>
-        <Link href={demo} className="">
+        <a href={demo} className="" target="_blank">
           <FaRegCirclePlay className="h-7 w-7 border rounded-full drop-shadow-md  bg-gradient-to-br dark:from-white dark:to-blue-500  cursor-pointer hover:scale-125 transition-all duration-100" />
-        </Link>
+        </a>
       </div>
-    </motion.div>
+    </>
   );
 }
 export default Project;
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,25 +1,53 @@
 "use client";
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Projects = () => {
-  const { ref } = useSectionInView("Projects", 0.5);
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-       <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
-       Works Recently Accomplished By Me
+    <section id="projects" className="scroll-mt-28 mb-28 ">
+      <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
+        Works Recently Accomplished By Me
       </p>
       <SectionHeading>My Projects</SectionHeading>
-      <div>
-        {projectsData.map((project, index) => (
-          <React.Fragment key={index}>
+
+      {projectsData.map((project, index) => {
+        const projectRef = useRef(null);
+        // const { ref: sectionInViewRef } = useSectionInView("Projects", 0.25);
+        const { ref: sectionInViewRef } = useSectionInView(
+          "Projects",
+          0.25,
+          index
+        );
+        useEffect(() => {
+          if (sectionInViewRef) {
+            sectionInViewRef(projectRef.current);
+          }
+        }, [sectionInViewRef]);
+        const { scrollYProgress } = useScroll({
+          target: projectRef,
+          offset: ["0 1", "1.33 1"],
+        });
+        const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+        const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+        return (
+          <motion.div
+            ref={projectRef}
+            style={{
+              scale: scaleProgress,
+              opacity: opacityProgress,
+            }}
+            className="group mb-3 sm:mb-8 last:mb-0 flex gap-2 sm:flex-row "
+            key={index}
+          >
             <Project {...project} />
-          </React.Fragment>
-        ))}
-      </div>
+          </motion.div>
+        );
+      })}
     </section>
   );
 };
