5943d6fc0ada99e5b229542e61e66834bdc567b5
Babina081 <babitamoo333@gmail.com>
2024-08-16 13:58:32 +0545

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import ThemeContextProvider from "@/context/theme-context";
 import CTA from "@/components/cta";
 import StarMouse from "@/components/theme-and-mouse/star-mouse";
 import CircleElasticMouse from "@/components/theme-and-mouse/circle-elastic-mouse";
-import Loading from "./loading";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -28,13 +27,13 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        {/* <Loading></Loading> */}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem]  -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header></Header>
-            <StarMouse></StarMouse> <CircleElasticMouse></CircleElasticMouse>
+            <StarMouse></StarMouse>
+            <CircleElasticMouse></CircleElasticMouse>
             {children}
             <Footer></Footer>
             <ThemeSwitch></ThemeSwitch>
