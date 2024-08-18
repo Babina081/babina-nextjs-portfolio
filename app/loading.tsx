448cb8693c2ce792f89dd50e863425bab5fc8c13
Babina081 <babitamoo333@gmail.com>
2024-08-18 15:58:02 +0545

    update loading color

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="spinner w-screen h-screen items-center justify-center flex m-auto"></div>
+    <div className="spinner w-screen h-screen items-center justify-center flex m-auto bg-gradient-to-b from-black to-blue-500 dark:from-white dark:to-blue-500 relative z-50"></div>
   );
 };
 
