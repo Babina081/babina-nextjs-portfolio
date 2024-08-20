36c1e0487090be8af03145de89112042f08ecaf6
Babina081 <babitamoo333@gmail.com>
2024-08-20 18:50:05 +0545

    add aceternity utils function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const validateString = (value: unknown, maxLength: number) => {
   if (!value || typeof value !== "string" || value.length > maxLength) {
     return false;
