fa84e4c739a35ddaa2b89567be74f995f4bca227
Babina081 <babitamoo333@gmail.com>
2024-08-31 09:20:21 +0545

    update loading layout

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,20 +1,28 @@
+"use client";
 import { SparklesCore } from "@/components/ui/sparkles";
+import { useTheme } from "@/context/theme-context";
 import React from "react";
 
 const Loading = () => {
+  const { theme } = useTheme();
   return (
-    <div className="spinner w-screen h-screen items-center justify-center flex m-auto bg-gradient-to-b from-black to-blue-500 dark:from-white dark:to-blue-500 relative z-50">
-      {" "}
-      <div className="w-full absolute inset-0 h-screen">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full "
-          particleColor="#FFFFFF"
-        />
+    <div className="fixed top-0 left-0 w-screen h-screen">
+      <div
+        className={`spinner w-screen h-screen items-center justify-center flex bg-gray-50 dark:bg-gray-900  relative z-50 text-gray-950  dark:text-opacity-90 `}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem]  -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>{" "}
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full "
+            particleColor={theme === "light" ? "#5B99C2" : "#FFFFFF"}
+          />
+        </div>
       </div>
     </div>
   );
