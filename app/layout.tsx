35f0b3fb70ab1d0012952f0b2402604cd434ca01
Babina081 <babitamoo333@gmail.com>
2024-08-31 09:20:38 +0545

    add loading wrapper in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import ThemeContextProvider from "@/context/theme-context";
 import CTA from "@/components/cta";
 import StarMouse from "@/components/theme-and-mouse/star-mouse";
 import CircleElasticMouse from "@/components/theme-and-mouse/circle-elastic-mouse";
+import LoadingWrapper from "@/components/theme-and-mouse/loading-wrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -29,18 +30,20 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem]  -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            <Header></Header>
-            <StarMouse></StarMouse>
-            <CircleElasticMouse></CircleElasticMouse>
-            {children}
-            <Footer></Footer>
-            <ThemeSwitch></ThemeSwitch>
-            <CTA></CTA>
-            <Toaster position="top-right"></Toaster>
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
+          <ThemeContextProvider>
+            <ActiveSectionContextProvider>
+        <LoadingWrapper>
+              <Header></Header>
+              <StarMouse></StarMouse>
+              <CircleElasticMouse></CircleElasticMouse>
+              {children}
+              <Footer></Footer>
+              <ThemeSwitch></ThemeSwitch>
+              <CTA></CTA>
+              <Toaster position="top-right"></Toaster>
+        </LoadingWrapper>
+            </ActiveSectionContextProvider>
+          </ThemeContextProvider>
       </body>
     </html>
   );
