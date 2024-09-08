5e9844e5edcd92502b002421aa7e51504eed3383
Babina081 <babitamoo333@gmail.com>
2024-09-08 19:37:46 +0545

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem]  -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-          <ThemeContextProvider>
-            <ActiveSectionContextProvider>
-        <LoadingWrapper>
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <LoadingWrapper>
               <Header></Header>
               <StarMouse></StarMouse>
               <CircleElasticMouse></CircleElasticMouse>
@@ -43,9 +43,9 @@ export default function RootLayout({
               <CTA></CTA>
               {/* <FloatingNav></FloatingNav> */}
               <Toaster position="top-right"></Toaster>
-        </LoadingWrapper>
-            </ActiveSectionContextProvider>
-          </ThemeContextProvider>
+            </LoadingWrapper>
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
