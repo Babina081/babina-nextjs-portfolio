1e76e42b5ac52181f5de9dd9fb1f6d766e2da2e2
Babina081 <babitamoo333@gmail.com>
2024-09-17 16:48:33 +0545

    update header

--- components/header.tsx
+++ components/header.tsx
@@ -5,31 +5,18 @@ import clsx from "clsx";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { Elastic, gsap, Power4 } from "gsap";
 
 const Header = () => {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
   // State to track if the audio is playing or not
   const [isPlaying, setIsPlaying] = useState(true);
+  const magnetoRef = useRef<HTMLButtonElement>(null);
+  const magnetoTextRef = useRef<HTMLSpanElement>(null);
 
   // Ref to store the audio element, with correct typing
   const audioRef = useRef<HTMLAudioElement | null>(null);
-
-  // Effect to handle automatic audio play when the component mounts
-  useEffect(() => {
-    const playAudio = async () => {
-      if (audioRef.current) {
-        try {
-          await audioRef.current.play();
-          setIsPlaying(true); // Update state to reflect that audio is playing
-        } catch (error) {
-          console.error("Autoplay was prevented by the browser:", error);
-          setIsPlaying(false); // Handle the case where autoplay is blocked
-        }
-      }
-    };
-    playAudio();
-  }, []);
 
   // Function to toggle play/pause of audio
   const toggleAudio = () => {
@@ -43,6 +30,62 @@ const Header = () => {
     }
   };
 
+  const activateMagneto = (event: MouseEvent) => {
+    const magneto = magnetoRef.current;
+    const magnetoText = magnetoTextRef.current;
+    if (!magneto || !magnetoText) return;
+
+    let boundBox = magneto?.getBoundingClientRect();
+    let magnetoToStrength = 30;
+    const magnetoTextStrength = 30;
+    const newX = (event.clientX - boundBox.left) / boundBox.width - 0.5;
+    const newY = (event.clientY - boundBox.top) / boundBox.height - 0.5;
+    gsap.to(magneto, {
+      duration: 1,
+      x: newX * magnetoToStrength,
+      y: newY * magnetoToStrength,
+      ease: Power4.easeOut,
+    });
+    gsap.to(magnetoText, {
+      duration: 1,
+      x: newX * magnetoTextStrength,
+      y: newY * magnetoTextStrength,
+      ease: Power4.easeOut,
+    });
+  };
+
+  const resetMagneto = (event: any) => {
+    const magneto = magnetoRef.current;
+    const magnetoText = magnetoTextRef.current;
+    if (!magneto || !magnetoText) return;
+    gsap.to(magneto, {
+      duration: 1,
+      x: 0,
+      y: 0,
+      ease: Elastic.easeOut,
+    });
+    gsap.to(magnetoText, {
+      duration: 1,
+      x: 0,
+      y: 0,
+      ease: Elastic.easeOut,
+    });
+  };
+
+  useEffect(() => {
+    const magneto = magnetoRef.current;
+    if (!magneto) return;
+    magneto.addEventListener("mousemove", activateMagneto);
+    magneto.addEventListener("mouseleave", resetMagneto);
+
+    return () => {
+      if (magneto) {
+        magneto.removeEventListener("mousemove", activateMagneto);
+        magneto.removeEventListener("mouseleave", resetMagneto);
+      }
+    };
+  }, []);
+
   return (
     <header className="z-[999] relative w-screen overflow-hidden  ">
       <motion.div
@@ -52,19 +95,20 @@ const Header = () => {
       ></motion.div>
       <nav className="flex fixed top-[0.6rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 ">
         <ul className="flex w-[30rem] flex-wrap items-center justify-center gap-y-1 text-[1.2rem] sm:text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-2 relative ">
+          {/* desktop */}
           {links.map((link) => (
             <motion.li
               key={link.hash}
-              className="h-3/4 hidden sm:flex items-center justify-center relative hover:bg-gradient-to-t from-blue-300 hover:rounded-full"
+              className="h-3/4 hidden sm:flex items-center justify-center relative hover:underline hover:text-gray-950 dark:hover:text-gray-200 hover:scale-105"
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 href={link.hash}
                 className={clsx(
-                  `flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 hover:underline  `,
+                  `flex w-full items-center justify-center px-3 py-3 transition dark:text-gray-500`,
                   {
-                    "text-gray-950 dark:text-gray-200 hover:no-underline":
+                    "bg-gradient-to-t from-blue-500 dark:from-blue-300 dark:to-white  rounded-full   text-gray-950 dark:text-gray-200 ":
                       activeSection === link.name,
                   }
                 )}
@@ -76,7 +120,7 @@ const Header = () => {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-gradient-to-t from-blue-500 rounded-full absolute inset-0 -z-10 dark:from-blue-300 dark:to-white"
+                    className="absolute inset-0 -z-10"
                     layoutId="activeSection"
                     transition={{ type: "spring", stiffness: 380, damping: 30 }}
                   ></motion.span>
@@ -85,6 +129,7 @@ const Header = () => {
             </motion.li>
           ))}
 
+          {/* mobile device */}
           {links.map((link) => (
             <motion.li
               key={link.hash}
@@ -121,12 +166,13 @@ const Header = () => {
             </motion.li>
           ))}
 
-          <div
-            className=" sm:h-12 sm:w-12 p-2 sm:absolute sm:-right-20 rounded-full flex items-center justify-center cursor-pointer border-white border-opacity-40  bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+          <button
+            className=" h-12 w-12 p-2 sm:absolute top-24 sm:top-0 sm:-right-20 rounded-full flex items-center justify-center cursor-pointer border-white border-opacity-40  bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
             onClick={toggleAudio}
+            ref={magnetoRef}
           >
-            <div className="box"></div>
-          </div>
+            <span className="box" ref={magnetoTextRef}></span>
+          </button>
         </ul>
       </nav>
       {/* Audio element */}
