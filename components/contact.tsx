6619341c302c1aa3ad685b8afc1c370a4d1af74a
Babina081 <babitamoo333@gmail.com>
2024-09-17 20:55:36 +0545

    update contact color contrast

--- components/contact.tsx
+++ components/contact.tsx
@@ -36,11 +36,11 @@ const Contact = () => {
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center max-w-[53rem] scroll-mt-28 "
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center max-w-[53rem] scroll-mt-28"
       id="contact"
     >
       <p className="section-description bg-gradient-to-b dark:from-white dark:to-blue-300   dark:text-transparent bg-clip-text ">
-        Let's Initiate our Converstion
+        Let&apos;s Initiate our Converstion
       </p>
       <SectionHeading>Contact Me</SectionHeading>
 
@@ -53,19 +53,19 @@ const Contact = () => {
           }
           toast.success("Email sent successfully!");
         }}
-        className="mt-10 flex flex-col dark:text-black/80"
+        className="mt-10 flex flex-col text-gray-900 dark:text-gray-100"
       >
         <input
           type="email"
           placeholder="Your email"
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none "
+          className="h-14 px-4 rounded-lg border border-gray-400 focus:border-gray-600 dark:border-gray-500 dark:focus:border-gray-300 dark:bg-gray-700 dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           required={true}
           maxLength={500}
           name="senderEmail"
         />
         <textarea
           placeholder="Your message"
-          className="borderBlack  h-52 my-3 rounded-lg p-4  dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          className="border border-gray-400 h-52 my-3 rounded-lg p-4 focus:border-gray-600 dark:border-gray-500 dark:focus:border-gray-300 dark:bg-gray-700 dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           maxLength={500}
           required
           name="message"
@@ -77,14 +77,14 @@ const Contact = () => {
         {contactDetails.map(({ name, icon, detail }) => (
           <div
             key={name}
-            className=" px-2 py-4 shadow-sm border-2  rounded-lg "
+            className="px-4 py-4 shadow-sm border-2 border-gray-400 dark:border-gray-600 rounded-lg dark:bg-gray-800"
           >
-            <h1 className="mb-2 flex items-center justify-center gap-2 w-full font-bold">
+            <h1 className="mb-2 flex items-center justify-center gap-2 w-full font-bold text-gray-900 dark:text-gray-100">
               <span>{icon}</span>
               {name}
             </h1>
 
-            <p className="text-sm">{detail}</p>
+            <p className="text-sm text-gray-700 dark:text-gray-400">{detail}</p>
           </div>
         ))}
       </div>
