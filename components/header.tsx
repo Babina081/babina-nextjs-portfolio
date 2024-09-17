0539ff4fc0e75a89bd5e8bfbd1c5fc3c4602015f
Babina081 <babitamoo333@gmail.com>
2024-09-17 17:50:51 +0545

    update audio toggle button for mobile device

--- components/header.tsx
+++ components/header.tsx
@@ -176,9 +176,8 @@ const Header = () => {
           <button
             className=" h-12 w-12 p-2 sm:absolute  sm:-right-20 rounded-full flex sm:hidden items-center justify-center cursor-pointer border-white border-opacity-40  bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
             onClick={toggleAudio}
-            ref={magnetoRef}
           >
-            <span className="box" ref={magnetoTextRef}></span>
+            <span className="box"></span>
           </button>
         </ul>
       </nav>
