41ed2cd51fe25c4baf76283f2e2b11d623e1ab69
Babina081 <babitamoo333@gmail.com>
2024-09-17 16:51:33 +0545

    update audio toggle button

--- components/header.tsx
+++ components/header.tsx
@@ -167,7 +167,7 @@ const Header = () => {
           ))}
 
           <button
-            className=" h-12 w-12 p-2 sm:absolute top-24 sm:top-0 sm:-right-20 rounded-full flex items-center justify-center cursor-pointer border-white border-opacity-40  bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+            className=" h-12 w-12 p-2 sm:absolute  sm:-right-20 rounded-full flex items-center justify-center cursor-pointer border-white border-opacity-40  bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
             onClick={toggleAudio}
             ref={magnetoRef}
           >
