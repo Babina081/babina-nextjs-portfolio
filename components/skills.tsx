d18e804f523b791bbe8dc76a1c80dca86cb17309
Babina081 <babitamoo333@gmail.com>
2024-09-17 19:10:07 +0545

    fix optimization

--- components/skills.tsx
+++ components/skills.tsx
@@ -288,14 +288,14 @@ const Skills = () => {
                 placeItems: "center",
               }}
             >
-             <Image
-  src={skill.image}
-  alt={skill.name}
-  width={50}
-  height={50}
-  style={{ width: "auto", height: "2rem" }} // Adjust height or width, and let the other scale naturally
-  layout="intrinsic"
-/>
+              <Image
+                src={skill.image}
+                alt={skill.name}
+                width={50}
+                height={50}
+                style={{ height: "auto" }} // Adjust height or width, and let the other scale naturally
+                layout="intrinsic"
+              />
             </li>
           ))}
         </ul>
