eb0080f81b323b2164b2793506176d2c18e03051
Babina081 <babitamoo333@gmail.com>
2024-09-17 19:42:42 +0545

    update demo aria label name

--- components/project.tsx
+++ components/project.tsx
@@ -65,12 +65,7 @@ function Project({
           {" "}
           <FaGithub className="h-7 w-7 border rounded-full drop-shadow-md  bg-gradient-to-br dark:from-white dark:to-blue-500    cursor-pointer hover:scale-125 transition-all duration-200" />
         </Link>
-        <a
-          href={demo}
-          target="_blank"
-          rel="noreferrer"
-          aria-label="Play demo video"
-        >
+        <a href={demo} target="_blank" rel="noreferrer" aria-label={demo}>
           <FaRegCirclePlay className="h-7 w-7 border rounded-full drop-shadow-md  bg-gradient-to-br dark:from-white dark:to-blue-500  cursor-pointer hover:scale-125 transition-all duration-100" />
         </a>
       </div>
