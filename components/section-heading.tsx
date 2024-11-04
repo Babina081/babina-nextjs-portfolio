ef7a21f28badf716d9b9f3e920a67d495c19a967
Babina081 <babitamoo333@gmail.com>
2024-11-04 18:43:49 +0545

    add grid background in skill section

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -5,7 +5,7 @@ type SectionHeadingProps = {
 
 const SectionHeading = ({ children }: SectionHeadingProps) => {
   return (
-    <h2 className=" capitalize mb-8  text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter textLight textDark">
+    <h2 className=" capitalize mb-8  text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter textLight textDark relative z-20">
       {children}
     </h2>
   );
--- components/sub-heading.tsx
+++ components/sub-heading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SubHeading = ({ children }: { children: React.ReactNode }) => {
-  return <p className="section-description textLight textDark">{children}</p>;
+  return <p className="section-description textLight textDark relative z-20">{children}</p>;
 };
 
 export default SubHeading;
